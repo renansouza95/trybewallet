2ffb9fcb1e39b172a03a6deadc3caf3347567d9a
Renan Souza <s.renan@outlook.com>
2022-01-10 20:37:54 -0300

    Reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,20 +1,16 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
 import { LOGIN } from '../actions';
-
-const INITIAL_STATE = {
-  user: {
-    email: '',
-    password: '',
-  },
-};
+import { INITIAL_STATE } from './initialState';
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case LOGIN:
     return {
       ...state,
-      email: action.email,
-      password: action.password,
+      user: {
+        email: action.email,
+        password: action.password,
+      },
     };
   default:
     return state;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,20 +1,15 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 import { GET_CURRENCY_SUCCESS, GET_CURRENCY_FAIL } from '../actions';
-
-const INITIAL_STATE = {
-  wallet: {
-    currencies: [],
-    expenses: [],
-  },
-  error: '',
-};
+import { INITIAL_STATE } from './initialState';
 
 const walletReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case GET_CURRENCY_SUCCESS:
     return {
       ...state,
-      currencies: action.currencies,
+      wallet: {
+        currencies: action.currencies,
+      },
     };
   case GET_CURRENCY_FAIL:
     return {
