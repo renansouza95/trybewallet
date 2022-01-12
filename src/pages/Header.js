3f4e4243529ac87c191717bdbf6b439b8e0a6142
Renan Souza <s.renan@outlook.com>
2022-01-12 17:40:31 -0300

    feat: Header component

---
+++ src/pages/Header.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <h1>TrybeWallet</h1>
+        <div className="user-info">
+          <span data-testid="email-field">
+            Email:
+            { email }
+          </span>
+          <span data-testid="total-field">
+            Despesa total: 0
+            {/* { expenses } */}
+          </span>
+          <span data-testid="header-currency-field">
+            BRL
+          </span>
+        </div>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  expenses: state.wallet.expenses,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  // expenses: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
